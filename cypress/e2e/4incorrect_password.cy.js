d0474bb06cbe48f67e4112ad13c3ddcafcae9349
T N Martins <47818453+thetoycompany@users.noreply.github.com>
2023-10-30 12:07:08 +0000

    e2e files

--- cypress/e2e/4incorrect_password.cy.js
+++ cypress/e2e/4incorrect_password.cy.js
@@ -1,6 +1,7 @@
 const lgnpage = () => {
   cy.visit('/')
   cy.request('/').its('status').should('eq', 200)
+  
 }
 
 
@@ -10,8 +11,13 @@ var fake_password = 'Testing'
 
 describe('template spec', () => {
   it('passes', () => {
-    lgnpage
-
+    lgnpage()
+    cy.get("#logIn").should('be.enabled');
+    cy.get('#username-container').type(valid_username)
+    cy.get('#password-container').type(fake_password)
+    cy.get("#logIn").click()
+    cy.wait(500)
+    cy.get('[data-qa-id="undefined-text"]').contains("We don't recognize that email and/or password")
   })
 })
 
